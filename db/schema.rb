6d04664d2e0881ad9dce728ec5685b10cc26dfc3
tamura-interplan-0215 <tamura.interplan.0215@gmail.com>
2023-08-02 19:25:58 +0000

    completed Lesson9

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_02_175046) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_02_185501) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
